26672eb66e5e1774fd377dd59fb5b554be8f2388
Gilles Castro <gillescastro1021@gmail.com>
2022-01-27 22:53:41 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const miniMain = document.getElementById('main');
+miniMain.remove();
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory');
+newHeader.innerHTML='YOUR-NAME is the champion';
+document.body.append(newHeader);
